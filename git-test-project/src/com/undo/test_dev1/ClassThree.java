d67e97d047dbebe92d590d6baf9c463487a14401
James <hpf301@163.com>
2019-06-30 15:13:56 +0800

    第三次提交，在dev1分支下多了一个包com.undo.test_dev1和ClassThree

---
+++ git-test-project/src/com/undo/test_dev1/ClassThree.java
@@ -0,0 +1,9 @@
+package com.undo.test_dev1;
+
+public class ClassThree {
+    public static void main(String[] args) {
+        System.out.println("当前分支dev1下的ClassThree");
+        System.out.println("当前分支dev1下的ClassThree");
+        System.out.println("当前分支dev1下的ClassThree");
+    }
+}
