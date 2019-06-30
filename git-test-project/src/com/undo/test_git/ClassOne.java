0812afa5dedcf0c02fb38443e93226a7dcc3f42c
James <hpf301@163.com>
2019-06-30 15:05:38 +0800

    从远程仓库clone的版本更新后提交

--- git-test-project/src/com/undo/test_git/ClassOne.java
+++ git-test-project/src/com/undo/test_git/ClassOne.java
@@ -1,6 +1,11 @@
 package com.undo.test_git;
 
 public class ClassOne {
+
+    private String name;
+    private int age;
+
+
     public static void main(String[] args) {
         System.out.println("this is test git------ClassOne");
         System.out.println("this is test git------ClassOne");
@@ -10,4 +15,6 @@ public class ClassOne {
     public void test1(){
         System.out.println("第一次提交后的修改，添加了一个方法");
     }
+
+
 }
