be60774263673ef2153e07e01c3d3c3b0c4a21c4
James <hpf301@163.com>
2019-06-30 15:20:12 +0800

    修改后push github

--- git-test-project/src/com/undo/test_git/ClassTwo.java
+++ git-test-project/src/com/undo/test_git/ClassTwo.java
@@ -1,9 +1,17 @@
 package com.undo.test_git;
 
 public class ClassTwo {
+
+    private String name;
+    private int age;
+
+
     public static void main(String[] args) {
         System.out.println("this is test git------ClassTwo");
         System.out.println("this is test git------ClassTwo");
         System.out.println("this is test git------ClassTwo");
     }
+
+
+
 }
