53a3efbe69b0e683ec8e311ec3b64b91ccae5d6e
James <hpf301@163.com>
2019-06-30 14:41:02 +0800

    第一次提交项目到本地仓库

---
+++ git-test-project/src/com/undo/test_git/ClassOne.java
@@ -0,0 +1,9 @@
+package com.undo.test_git;
+
+public class ClassOne {
+    public static void main(String[] args) {
+        System.out.println("this is test git------ClassOne");
+        System.out.println("this is test git------ClassOne");
+        System.out.println("this is test git------ClassOne");
+    }
+}
---
+++ git-test-project/src/com/undo/test_git/ClassTwo.java
@@ -0,0 +1,9 @@
+package com.undo.test_git;
+
+public class ClassTwo {
+    public static void main(String[] args) {
+        System.out.println("this is test git------ClassTwo");
+        System.out.println("this is test git------ClassTwo");
+        System.out.println("this is test git------ClassTwo");
+    }
+}
